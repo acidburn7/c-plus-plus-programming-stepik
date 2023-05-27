7f29c844dfeb25201d0ceb05bc2f07ac62cf57ff
acidburn7 <varaksinaalla@mail.ru>
2023-05-27 23:32:27 +0300

    using namespace std

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
   int i = 42;
   double d = 3.14;
   const char *s = "C-style string";
 
-  std::cout << "This is an integer " << i << "\n";
-  std::cout << "This is a double " << d << "\n";
-  std::cout << "This is a \"" << s << "\"\n";
+  cout << "This is an integer " << i << "\n";
+  cout << "This is a double " << d << "\n";
+  cout << "This is a \"" << s << "\"\n";
 
   return 0;
 }
